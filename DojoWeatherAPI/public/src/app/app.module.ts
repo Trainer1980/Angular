6faf014548bfcfbf0493aa0a8519aed5425861bb
Holly Howell <hollyhowell1980@gmail.com>
2020-04-07 15:31:51 -0500

    new projects

--- DojoWeatherAPI/public/src/app/app.module.ts
+++ DojoWeatherAPI/public/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HttpService } from './http.service';
